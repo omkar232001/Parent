d2689284a3c7ea6771c8c35c53f644578b83079d
omkar232001 <budhawaleomkar01@gmail.com>
2024-08-18 16:46:28 +0530

    Update abs.java

--- abs.java
+++ abs.java
@@ -2,14 +2,14 @@ package Abstraction;
 abstract class e{
     abstract int r(int a,int b);
     void tr(){
-        System.out.println("hi");
+        System.out.println("Method");
     }
 }
 class we extends e{
     @Override
     int r(int a,int b) {
         int t=a+b;
-        System.out.println("hello" +t);
+        System.out.println("Addition =" +t);
         return t;
     }
 }
